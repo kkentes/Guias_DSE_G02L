d4e5aa6e6735d59c09cc5848c4f757cf0029dd19
kkentes <kevin.fuentesk@gmail.com>
2022-07-28 18:06:57 -0600

    Guia1_Ejercicios

---
+++ Guia1/MvcPelicula/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPelicula.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(string nombre, string apellido, int numVeces = 1)
+        {
+            ViewBag.NumVeces = numVeces;
+
+            int cont = 1;
+            ViewBag.cont = cont;
+
+            ViewBag.Mensaje= "Hola "+nombre + " " + apellido + "," + " veces mostrado = ";
+                       
+            return View();
+        }
+    }
+}
---
+++ Guia1/Persona/Models/Persona.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Persona.Models
+{
+    public class Persona
+    {
+        public int DUI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechadeNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+
+    }
+
+    public class DatosPersonas : DbContext
+    {
+        public DbSet<Persona> Personas { get; set; }
+    }
+}
